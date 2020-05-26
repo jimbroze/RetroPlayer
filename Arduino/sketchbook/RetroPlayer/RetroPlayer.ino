6f73cf2d78201bbf8fc35f6d771db6eb97307826
Jimbroze <james.n.dickinson@gmail.com>
2020-05-26 19:45:03 +0100

    Fixed some TODOs

--- Arduino/sketchbook/RetroPlayer/RetroPlayer.ino
+++ Arduino/sketchbook/RetroPlayer/RetroPlayer.ino
@@ -1,11 +1,11 @@
 
-#include <string>
+// #include <string>
 #include <ArduinoJson.h>
 #include "LowPower.h"
 #include "PCF8523.h"
 #include "SleepyPi2.h"
 
-#define DEBOUNCE 10  // button debouncer, how many ms to debounce, 5+ ms is usually plenty
+const byte DEBOUNCE = 10;  // button debouncer, how many ms to debounce, 5+ ms is usually plenty
  
 const byte POWER_SWITCH_PIN = 2;	// Pin B0 - Arduino 8
 const byte DOOR_PIN = 3;	// Pin B0 - Arduino 8
@@ -13,121 +13,65 @@ const unsigned int POWER_ON_TIMEOUT = 30; // Seconds
 const unsigned int HANDSHAKE_TIMEOUT = 30; // Seconds
 
 byte inputs[] = {2, 3, 5, 6, 8, 11, 12};
-#define NUM_INPUTS sizeof(inputs);
-byte inputState[NUM_INPUTS];
-
 byte outputs[] = {7, 10, 13};
-#define NUM_OUTPUTS sizeof(outputs);
-byte outputState[NUM_OUTPUTS];
+byte analogues[][2] = {"A0", "A1", "A4", "A5"};
+byte ANALOGUE_SINK = 9;
+byte ANALOGUE_AVERAGES = 6; // Even no. so that jumps between 2 numbers are centralised
 
-byte analogues[] = {"A0", "A1", "A4", "A5"};
-
-char* serialKeys[] = {
-    "display",
-    ""
+// Serial OUT
+const char serialKeys[][7] = {
+    "disp",
+    "val2"
 };
-byte SERIAL_ARRAY_SIZE = sizeof(serialKeys);
+byte disp = 1;
+byte val2 = 1;
+byte *handshakeData[] = {
+    &disp,
+    &val2
+};
+// Single values:
+// errSer = Serial error (String)
+
+// Serial IN
+const int SERIAL_DICT_CAPACITY = JSON_OBJECT_SIZE(2); // + JSON_ARRAY_SIZE(2) If size 3 object includes size 2 array
+const byte numChars = 64; //Increase if required for more data.
+// Serial buffer is 64 bytes but program should read faster than buffer
+
+// TODO Can values change without function?? i.e state machine checks values. Much simpler
+void pi_on(byte data) {
+    awake = data;
+}
+void funcB(byte) {
+    return;
+}
+struct command {
+    char* funcName;
+    void (*function)(byte);
+};
+struct command commands[] = {
+    {"piOn", pi_on},
+    {"strB", funcB},
+    {0,0}
+};
 
 byte arduinoState = 50;
 byte piState = 50;
 byte displayOn = 0;
 byte piAwake = 0;
 
+const byte NUM_INPUTS = sizeof(inputs);
+byte inputState[NUM_INPUTS];
 
-// ************************* SERIAL FUNCS *************************
+const byte NUM_OUTPUTS = sizeof(outputs);
+byte outputState[NUM_OUTPUTS];
 
-void read_serial_data() {
+const byte NUM_ANALOGUES = sizeof(analogues);
+byte analogueState[NUM_ANALOGUES];
 
-    if (Serial.available() <= 0) {
-        return;
-    }
-    // read the incoming byte:
-    int inData = Serial.read();
+const byte SERIAL_ARRAY_SIZE = sizeof(serialKeys);
 
-    StaticJsonDocument<256> doc;
-    DeserializationError error = deserializeJson(doc, inData);
-    if (error) {
-        Serial.println(error.c_str()); 
-        return;
-    }
-    // Get a reference to the root object
-    JsonObject obj = doc.as<JsonObject>();
-    // Loop through all the key-value pairs in obj
-    for(auto p: obj) {
-//        p.key(); // is a JsonString
-//        p.value(); // is a JsonVariant
-    }
 
-    // TODO Set pi state
-}
 
-void send_serial_value(const char *key, byte value) {
-    const int capacity=JSON_OBJECT_SIZE(1);
-    StaticJsonDocument<capacity> doc;
-    
-    doc[key] = value;
-    // Produce a minified JSON document
-    serializeJson(doc, Serial);
-    serial.println();
-}
-
-void send_serial_dict(byte data[]) {
-    const int capacity=JSON_OBJECT_SIZE(SERIAL_ARRAY_SIZE);
-    StaticJsonDocument<capacity> doc;
-    string output;
-
-    for (byte i = 0; i < SERIAL_ARRAY_SIZE; i++)
-        doc[serialKeys[i]] = data[i];
-    // Produce a minified JSON document
-    serializeJson(doc, Serial);
-    serial.println();
-}
-
-// ************************* HARDWARE IO *************************
-
-void check_inputs() {
-    static byte newInputState[NUM_INPUTS];
-    static byte debounce[NUM_INPUTS];
-    static long lastTime[NUM_INPUTS];
-  
-    for (byte i = 0; i < NUM_INPUTS; i++) {
-        if (millis() < lastTime[i]) {
-            lastTime[i] = millis(); // Timer has wrapped around
-        }
- 
-        if ((lastTime[i] + DEBOUNCE) > millis()) {
-            continue; // not enough time has passed to debounce
-        }
-        // DEBOUNCE time passed or not started
-
-        newInputState[i] = digitalRead(inputs[i]);   // read the input
-
-        if (newInputState[i] == inputState[i]) {
-            if (debounce[i] == 1)  {
-                debounce[i] = 0; // Debounce failed, reset
-            }
-            continue; // Debounced failed or not started
-        }
-        if (debounce[i] == 1) {
-            debounce[i] = 0; // Debouncing finished, stop debounce
-            inputState[i] = newInputState[i];
-            inputCallbacks [i] (inputState[i]);
-        } else {
-            debounce[i] = 1; // Debounce not started, start debounce
-        }
-        // Input changed. Reset timer
-        lastTime[i] = millis();
-    }
-}
-
-void check_analogues() {
-    // static byte debounce;
-    for (byte i=0; i< NUM_ANALOGUES; i++) {
-        int reading = analogRead(analogues[i]);
-        delay(10);
-        // TODO Filter??
-    }
-}
 
 // ************************* CALLBACK HANDLERS *************************
 
@@ -174,20 +118,162 @@ ioFunction inputCallbacks [] = {
 };
 
 
+// ************************* SERIAL FUNCS *************************
+
+void read_serial_data() {
+    static byte ndx = 0; // Index
+    char endMarker = '\n';
+    char dataIn;
+    char receivedChars[numChars]; 
+
+    // while (Serial.available() > 0 && newData == false) {
+    while (Serial.available() > 0) {
+        dataIn = Serial.read();
+
+        if (dataIn != endMarker) {
+            receivedChars[ndx] = dataIn;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            // newData = true;
+            process_serial_data(receivedChars);
+        }
+    }
+
+}
+
+void process_serial_data(char serialData[]) {
+    StaticJsonDocument<SERIAL_DICT_CAPACITY> doc;
+    DeserializationError error = deserializeJson(doc, serialData);
+    if (error) {
+        send_serial_string("errSer", error.c_str());
+        return;
+    }
+    // Get a reference to the root object
+    JsonObject obj = doc.as<JsonObject>();
+    // Loop through all the key-value pairs in obj
+
+    for(auto p: obj) {
+        struct command *scan;
+        for(scan = commands; scan->function; scan++) {
+            if(!strcmp(p.key().c_str(), scan->funcName)) {
+                scan->function(p.value());
+                break;
+            }
+            // TODO Look for 0?
+        }
+    }
+}
+
+void send_serial_value(const char *key, byte value) {
+    const int capacity=JSON_OBJECT_SIZE(1);
+    StaticJsonDocument<capacity> doc;
+    
+    doc[key] = value;
+    // Produce a minified JSON document
+    serializeJson(doc, Serial);
+    Serial.println();
+}
+
+void send_serial_string(const char *key, const char *value) {
+    const int capacity=JSON_OBJECT_SIZE(1);
+    StaticJsonDocument<capacity> doc;
+    
+    doc[key] = value;
+    // Produce a minified JSON document
+    serializeJson(doc, Serial);
+    Serial.println();
+}
+
+void send_serial_dict() {
+    const int capacity=JSON_OBJECT_SIZE(SERIAL_ARRAY_SIZE);
+    StaticJsonDocument<capacity> doc;
+
+    for (byte i = 0; i < SERIAL_ARRAY_SIZE; i++)
+        doc[serialKeys[i]] = *handshakeData[i];
+    // Produce a minified JSON document
+    serializeJson(doc, Serial);
+    Serial.println();
+}
+
+// ************************* HARDWARE IO *************************
+
+void check_inputs() {
+    static byte newInputState[NUM_INPUTS];
+    static byte debounce[NUM_INPUTS];
+    static long lastTime[NUM_INPUTS];
+  
+    for (byte i = 0; i < NUM_INPUTS; i++) {
+        if (millis() < lastTime[i]) {
+            lastTime[i] = millis(); // Timer has wrapped around
+        }
+ 
+        if ((lastTime[i] + DEBOUNCE) > millis()) {
+            continue; // not enough time has passed to debounce
+        }
+        // DEBOUNCE time passed or not started
+
+        newInputState[i] = digitalRead(inputs[i]);   // read the input
+
+        if (newInputState[i] == inputState[i]) {
+            if (debounce[i] == 1)  {
+                debounce[i] = 0; // Debounce failed, reset
+            }
+            continue; // Debounced failed or not started
+        }
+        if (debounce[i] == 1) {
+            debounce[i] = 0; // Debouncing finished, stop debounce
+            inputState[i] = newInputState[i];
+            inputCallbacks[i] (inputState[i]);
+        } else {
+            debounce[i] = 1; // Debounce not started, start debounce
+        }
+        // Input changed. Reset timer
+        lastTime[i] = millis();
+    }
+}
+
+void check_analogues() {
+    static int throwaway;
+    // static byte debounce;
+    for (byte i=0; i< NUM_ANALOGUES; i++) {
+        // Ignore first reading to allow capacitor to charge. Especially high impedance pot
+        throwaway = analogRead(analogues[i]);
+        byte analogueValue = analogRead(analogues[i]);
+        for(byte j=0; j < ANALOGUE_AVERAGES; j++) { 
+            delay(10);
+            analogueValue = analogRead(analogues[i]);
+        }
+        analogueState[i] = analogueValue / ANALOGUE_AVERAGES;
+        // TODO Filter??
+    }
+}
+
+
 // ************************* STATE MACHINES *************************
 
 
 void pi_controller() {
     static byte handshakeStatus;
+    static long lastHandshakeTime;
+    static bool piPower;
     switch(piState) { // State machine to control power up sequence
         case 10: // Shutdown
             // Send shutdown signal
         case 20: // Shutting down
             //TODO Check current. When confirmed off:
-                // piState = 50;
+            piPower = SleepyPi.checkPiStatus(false);  // Don't Cut Power automatically
+            if (piPower == false) {
+                piState = 30;
                 // TODO Clear serial buffer?
-                // SleepyPi.enablePiPower(false); 
-                // piState = 30;
+                SleepyPi.enablePiPower(false); 
+                piState = 50;
+            }
         case 50: // Pi is off
             // 
         case 110: // Wakeup Pi
@@ -196,14 +282,16 @@ void pi_controller() {
         case 120: // Wait for Pi wakeup confirmation
             if (piAwake == 1) {
                 piState = 130;
-                handshakeStatus = 0
+                handshakeStatus = 1;
                 // Send pi initial data
-                //TODO Send serial data here
+                send_serial_dict();
+                //TODO Add timeout
             }
         case 130: // Wait for handshake confirmation
-            if (handshakeStatus == 1) {
+            if (handshakeStatus == 2) {
                 lastHandshakeTime = millis();
                 piState = 150;
+                // FIXME two handshake states?
             }
         case 140: // Handshake confirmed. Check time since last message
             if ( (lastHandshakeTime + HANDSHAKE_TIMEOUT) > millis() ) {
@@ -213,7 +301,7 @@ void pi_controller() {
             }
 
         case 200: // Maintenence mode
-
+            break;
     }
 }
 
@@ -250,9 +338,9 @@ void arduino_controller() {
             }
             // TODO If inputs tripped, turn display on
         case 120: // Woken up, display should be on
-            displayOn = 1
+            displayOn = 1;
             if (piState > 120) { // Initial data already sent. Send display on seperate
-                send_serial_value("display", 1)
+                send_serial_value("disp", 1);
             }
             arduinoState = 130;
         case 150: // Standard awake state
@@ -260,11 +348,12 @@ void arduino_controller() {
         case 200: // Maintenence mode request
             // Turn off analogue source and sink
             SleepyPi.enableExtPower(false);
-            pinMode(OUT_SINK, INPUT);
-            send_serial_value("state", arduinoState)
+            pinMode(ANALOGUE_SINK, INPUT);
+            send_serial_value("state", arduinoState);
             arduinoState = 202;
 
         case 202:  // In maintenence mode
+            break;
     }
 }
 
@@ -297,10 +386,11 @@ void setup() {
 }
 
 void loop() {
-    arduino_controller() // State machine to handle arduino power
-    pi_controller() // State machine to handle pi power and handshaking
+    arduino_controller(); // State machine to handle arduino power
+    pi_controller(); // State machine to handle pi power and handshaking
     if (arduinoState > 100) { // If arduino is on, read inputs
         check_inputs();
+        check_analogues();
     }
     if (piState > 100) { // If pi is on, read serial
         read_serial_data();
